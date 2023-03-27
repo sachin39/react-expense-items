f200d8ba117d827d7679338e69a67c4f672d7a9a
sachin39 <sachin.kr.39@gmail.com>
2023-03-27 18:27:02 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+
+  const products = [
+    {
+    title: "Car",
+    price: 2000,
+    date: new Date(2023, 3, 27)
+  },
+  {
+    title: "TV",
+    price: 500,
+    date: new Date(2022, 12, 25)
+  },
+  {
+    title: "Wooden Table",
+    price: 200,
+    date: new Date(2023, 2, 10)
+  },
+]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ExpenseItem 
+        title={products[0].title}
+        price={products[0].price}
+        date={products[0].date}
+      ></ExpenseItem>
+      <ExpenseItem 
+        title={products[1].title}
+        price={products[1].price}
+        date={products[1].date}
+      ></ExpenseItem>
+      <ExpenseItem 
+        title={products[2].title}
+        price={products[2].price}
+        date={products[2].date}
+      ></ExpenseItem>
+      
     </div>
   );
 }
