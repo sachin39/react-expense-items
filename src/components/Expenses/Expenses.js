d31e0f445832d6ddc5abd6651fb33b30fd249f85
sachin39 <sachin.kr.39@gmail.com>
2023-05-23 15:43:48 +0530

    Conditional return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("2020");
@@ -11,28 +10,22 @@ const Expenses = (props) => {
   const filterChainHandler = (selectedFilter) => {
     setFilterYear(selectedFilter);
   };
-  const filteredItems = props.items.filter((item) => {
-    return item.date.getFullYear().toString() === filterYear;
+  const filteredItems = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
   });
 
   return (
-    <div>
-      <Card className="expenses">
-        <ExpenseFilter
-          selected={filterYear}
-          onChangeFilter={filterChainHandler}
-        />
-
-        {filteredItems.map((item) => (
-          <ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
+    <li>
+      <div>
+        <Card className="expenses">
+          <ExpenseFilter
+            selected={filterYear}
+            onChangeFilter={filterChainHandler}
           />
-        ))}
-      </Card>
-    </div>
+          <ExpensesList items={filteredItems} />
+        </Card>
+      </div>
+    </li>
   );
 };
 
